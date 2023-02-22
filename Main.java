72b8d6100e545acf84335e268a0a140d82e0a7a3
Madhu12Mita <124993929+Madhu12Mita@users.noreply.github.com>
2023-02-22 22:53:57 +0530

    changes made

--- Main.java
+++ Main.java
@@ -3,127 +3,49 @@
 // importing the required packages
 import java.io.*;
 import java.util.*;
-// Create a file
-class CreateFile
+class MyFile
 {
-    // parameters are name of the file you want to create
-    public void create(String filename)
+    /**
+     * create a File class Object so that it is accessible to every method
+     */
+    File file;
+
+    /**
+     * Creates a file if doesn't exist
+     * @param filename String
+     */
+    MyFile(String filename)
     {
-        try
-        {
+        try {
             // File object is created for the passed fielname
-            File f=new File(filename);
+            this.file = new File(filename);
             // checks if file already exits , if no creates new file
-            if(f.createNewFile())
-            {
+            if (this.file.createNewFile()) {
                 System.out.println("file created");
             }
             // if file is present it doesnot create duplicate file
-            else
-            {
+            else {
                 System.out.println("File exists");
             }
-        }
-        catch(IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("Error in creating file");
         }
     }
-}
-// updating the entire file
-class Update
-{
-    // parameters are name of the file you want to update
-    public void write(String filename)
-    {
-        try
-        {
-            File f1=new File(filename);
-            // if file is not present , it creates a new file
-            if(!f1.exists())
-            {
-                f1.createNewFile();
-            }
-            Scanner sc=new Scanner(System.in);
-            // writing to new file
-            FileWriter fwrite =new FileWriter(f1.getName(),true);
-            BufferedWriter bw= new BufferedWriter(fwrite);
-            System.out.println("Enter the text");
-            String text=sc.nextLine();
-            bw.write(text);
-            bw.newLine();
-            bw.close();
-            System.out.println("Written");
-        }
-        catch(IOException e)
-        {
-            System.out.println("Write Exception");
-        }
-    }
-}
-// reading from the file
-class Reader
-{
-    // parameters are name of the file you want to read
-    public void read(String filename)
-    {
-        try
-        {
-            File f=new File(filename);
-            // checks if file exists or not
-            if(f.exists())
-            {
-                Scanner sc=new Scanner(f);
-                // reads line to line by checking if line is present or not
-                while(sc.hasNextLine())
-                {
-                    System.out.println(sc.nextLine());
-                }
-            }
-            // if file doesn't exist
-            else
-            {
-                System.out.println("file doesn't exist");
-            }
-        }
-        catch(IOException e)
-        {
-            System.out.println("Reading error");
-        }
-    }
-}
-// delete the file
-class DeleteFile
-{
-    // parameters are name of the file you want to delete
 
-    public void deleteFile(String filename)
-    {
-        File file= new File(filename);
-        if (file.delete())
-        {
-            System.out.println("File deleted successfully");
-        }
-        else
-        {
-            System.out.println("Failed to delete the file");
-        }
-    }
-}
-// update a particular line in a file
-
-class UpdateLine
-{
-    // parameters are name of the file and the new text you want to update in the given file
-    public void update(String filename,String new_text)
+    /**
+     * This method updates a line in a file at the given index
+     * @param new_text String
+     * @param index int
+     * @return boolean value to indicate the success of operation
+     */
+    public boolean UpdateLine(String new_text, int index)
     {
         try
         {
-            File f=new File(filename);
             // to check if file exists or not
-            if(f.exists())
+            if(this.file.exists())
             {
-                Scanner sc=new Scanner(f);
+                Scanner sc=new Scanner(this.file);
                 ArrayList<String> text=new ArrayList<>();
                 while(sc.hasNextLine())
                 {
@@ -141,7 +63,7 @@ class UpdateLine
                     {
                         str.append(i+"\n");
                     }
-                    FileWriter fwrite=new FileWriter(filename);
+                    FileWriter fwrite=new FileWriter(this.file);
                     fwrite.write(String.valueOf(str));
                     fwrite.close();
                 }
@@ -162,6 +84,130 @@ class UpdateLine
         }
     }
 
+    /**
+     * this method overwrites the file with the given content
+     * @param new_text String
+     * @return boolean value to indicate the success of operation
+     */
+    public boolean WriteFile(String new_text)
+    {
+        try
+        {
+            // if file is not present , it creates a new file
+            if(!this.file.exists())
+            {
+                System.out.println("File doesn't exist");
+                return false;
+            }
+            Scanner sc=new Scanner(System.in);
+            // writing to new file
+            FileWriter fwrite =new FileWriter(f1.getName(),true);
+            BufferedWriter bw= new BufferedWriter(fwrite);
+            System.out.println("Enter the text");
+            String text=sc.nextLine();
+            bw.write(text);
+            bw.newLine();
+            bw.close();
+            System.out.println("Written");
+            return true;
+        }
+        catch(IOException e)
+        {
+            System.out.println("Write Exception");
+        }
+    }
+
+    /**
+     * This method writes the content at the given line
+     * @param content
+     * @param index
+     * @return
+     */
+    public boolean WriteLine(String content,int index)
+    {
+
+    }
+
+    /**
+     * this methods reads the line at given index line and prints the same
+     * @param index int
+     * @return boolean value to indicate the success of operation
+     */
+    public boolean readLine(int index)
+    {
+
+    }
+
+    /**
+     * this method deletes the file
+     * @return boolean value to indicate the success of operation
+     */
+    public boolean deleteFile()
+    {
+        if (this.file.delete())
+        {
+            System.out.println("File deleted successfully");
+            return true;
+        }
+        else
+        {
+            System.out.println("Failed to delete the file");
+            return false;
+        }
+    }
+
+    /**
+     * this method inserts a line at given index
+     * @param new_line String
+     * @param index int
+     * @return boolean value to indicate the success of operation
+     */
+    public boolean WriteLine(String new_line,int index)
+    {
+
+    }
+
+    /**
+     * This method deletes the line at the given line index
+     * @param index int
+     * @return boolean value to indicate the success of operation
+     */
+    public boolean deleteLine(int index)
+    {
+
+    }
+
+    /**
+     *  this method reads the line and prints the same
+     * @return boolean value to indicate the success of operation
+     */
+    public boolean readFile()
+    {
+        try
+        {
+            // if file is not present , it creates a new file
+            if(!this.file.exists())
+            {
+                System.out.println("File doesn't exist");
+                return false;
+            }
+            Scanner sc=new Scanner(System.in);
+            // writing to new file
+            FileWriter fwrite =new FileWriter(this.file.getName(),true);
+            BufferedWriter bw= new BufferedWriter(fwrite);
+            System.out.println("Enter the text");
+            String text=sc.nextLine();
+            bw.write(text);
+            bw.newLine();
+            bw.close();
+            System.out.println("Written");
+            return true;
+        }
+        catch(IOException e)
+        {
+            System.out.println("Write Exception");
+        }
+    }
 }
 //starting point of execution
 public class Main
@@ -171,57 +217,7 @@ public class Main
         //menu-driven code
         while(true)
         {
-            System.out.println("1.Create a file\n2.Read a file\n3.Update to particular line in a file\n4.Update a file\n5.Delete the file\n6.End");
-            Scanner sc=new Scanner(System.in);
-            System.out.println("Enter your choice: ");
-            int choice=sc.nextInt();
-            switch(choice)
-            {
-                //create a file
-                case 1:
-                    System.out.println("Creating File");
-                    System.out.println("Enter filename");
-                    String filename=sc.next();
-                    CreateFile c=new CreateFile();
-                    c.create(filename);
-                    break;
-                // reading a file
-                case 2:
-                    Reader r= new Reader();
-                    System.out.println("Enter filename");
-                    String rfile=sc.next();
-                    r.read(rfile);
-                    break;
-                //specific line updation
-                case 3:
-                    Scanner s=new Scanner(System.in);
-                    System.out.println("Enter the new text to be updated");
-                    String new_text=s.nextLine();
-                    UpdateLine u=new UpdateLine();
-                    System.out.println("Enter filename");
-                    String ufile=sc.next();
-                    u.update(ufile,new_text);
-                    break;
-                //update the entire file
-                case 4:
-                    System.out.println("Enter filename");
-                    String file=sc.next();
-                    Update w=new Update();
-                    w.write(file);
-                    break;
-                //deleting the entire file
-                case 5:
-                    DeleteFile d= new DeleteFile();
-                    System.out.println("Enter filename");
-                    String dfile=sc.next();
-                    d.deleteFile(dfile);
-                    break;
-                //end of process
-                case 6:
-                    System.exit(0);
-            }
+
         }
-
     }
-
 }
